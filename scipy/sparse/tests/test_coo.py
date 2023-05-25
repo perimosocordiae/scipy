ea949c7d548dd607de07d03d2079fd1df620e6ce
CJ Carey <perimosocordiae@gmail.com>
2023-05-25 10:56:49 -0700

    Generalize some tests

--- scipy/sparse/tests/test_coo.py
+++ scipy/sparse/tests/test_coo.py
@@ -3,27 +3,57 @@ import pytest
 from scipy.sparse import coo_array
 
 
-def test_1d_shape_constructor():
+def test_shape_constructor():
     empty1d = coo_array((3,))
     assert empty1d.shape == (3,)
     assert np.array_equal(empty1d.toarray(), np.zeros((3,)))
 
+    empty2d = coo_array((3, 2))
+    assert empty2d.shape == (3, 2)
+    assert np.array_equal(empty2d.toarray(), np.zeros((3, 2)))
 
-def test_1d_dense_constructor():
-    res = coo_array([1, 2, 3])
-    assert res.shape == (3,)
-    assert np.array_equal(res.toarray(), np.array([1, 2, 3]))
-
-
-def test_1d_dense_constructor_with_shape():
-    res = coo_array([1, 2, 3], shape=(3,))
-    assert res.shape == (3,)
-    assert np.array_equal(res.toarray(), np.array([1, 2, 3]))
+    empty3d = coo_array((3, 2, 2))
+    assert empty3d.shape == (3, 2, 2)
+    with pytest.raises(ValueError,
+                       match='Cannot densify higher-rank sparse array'):
+        empty3d.toarray()
 
 
-def test_1d_dense_constructor_with_inconsistent_shape():
+def test_dense_constructor():
+    res1d = coo_array([1, 2, 3])
+    assert res1d.shape == (3,)
+    assert np.array_equal(res1d.toarray(), np.array([1, 2, 3]))
+
+    res2d = coo_array([[1, 2, 3], [4, 5, 6]])
+    assert res2d.shape == (2, 3)
+    assert np.array_equal(res2d.toarray(), np.array([[1, 2, 3], [4, 5, 6]]))
+
+    res3d = coo_array([[[3]], [[4]]])
+    assert res3d.shape == (2, 1, 1)
+
+
+def test_dense_constructor_with_shape():
+    res1d = coo_array([1, 2, 3], shape=(3,))
+    assert res1d.shape == (3,)
+    assert np.array_equal(res1d.toarray(), np.array([1, 2, 3]))
+
+    res2d = coo_array([[1, 2, 3], [4, 5, 6]], shape=(2, 3))
+    assert res2d.shape == (2, 3)
+    assert np.array_equal(res2d.toarray(), np.array([[1, 2, 3], [4, 5, 6]]))
+
+    res3d = coo_array([[[3]], [[4]]], shape=(2, 1, 1))
+    assert res3d.shape == (2, 1, 1)
+
+
+def test_dense_constructor_with_inconsistent_shape():
     with pytest.raises(ValueError, match='inconsistent shapes'):
         coo_array([1, 2, 3], shape=(4,))
+
+    with pytest.raises(ValueError, match='inconsistent shapes'):
+        coo_array([1, 2, 3], shape=(3, 1))
+
+    with pytest.raises(ValueError, match='inconsistent shapes'):
+        coo_array([[1, 2, 3]], shape=(3,))
 
 
 def test_1d_sparse_constructor():
